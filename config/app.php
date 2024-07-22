a42cc973bb2f90ec7111e42d14d2da5a3f1bbfdf
jesus <jesusjsg03@gmail.com>
2024-07-21 20:45:25 -0400

    se eliminaron los node modules

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('URL', 'http://localhost/nomina');
+
+    define('HOST', 'localhost');
+    define('DB', 'gestion_transporte');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('CHARSET', 'utf8mb4');
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('ignore_repeated_errors', true);
+    ini_set('display_errors', false);
+    ini_set('log_errors', true);
+    ini_set('error_log', '/laragon/www/nomina/php-errors.log');
+    error_log('Inicio de aplicacion web');
+?>
