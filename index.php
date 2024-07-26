e83b69e22b92b6b78ad0c659ae9c836c5bd67587
jesus <jesusjsg03@gmail.com>
2024-07-25 23:36:54 -0400

    configurar el gitignore, y se agregaron los includes en el index

--- index.php
+++ index.php
@@ -4,23 +4,27 @@
     ini_set('display_errors', false);
     ini_set('log_errors', true);
     ini_set('error_log', '/laragon/www/nomina/php-errors.log');
-    //error_log('Inicio del sistema de gestion de transporte');
+    error_log('Inicio del sistema de gestion de transporte');
 
-    require_once 'config/app.php';
+    require_once './config/app.php';
+    require_once './src/helpers/session_start.php';
+
+    if(isset($_GET['views'])){
+        $url = explode("/", $_GET['views']);
+    } else {
+        $url = ['login'];
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="<?= URL; ?>public/img/clover-logo-tab.ico">
-    <link rel="stylesheet" type="text/css" href="<?= URL; ?>public/css/main.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?= URL; ?>public/css/libs/bootstrap.min.css">
-    <title><?= APP_NAME; ?></title>
+    <?php require_once "./src/helpers/includes/header.php"; ?>
 </head>
 <body>
-    
+
+
+
+    <?php require_once "./src/helpers/includes/script.php"; ?>
 </body>
 </html>
