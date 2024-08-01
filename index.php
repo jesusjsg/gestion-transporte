c3ea6ac04203359ac685b3fcff875488d677dac7
jesus <jesusjsg03@gmail.com>
2024-07-31 21:56:05 -0400

    se agrego la imagen del login y sus estilos

--- index.php
+++ index.php
@@ -6,14 +6,21 @@
     ini_set('error_log', '/laragon/www/nomina/php-errors.log');
     error_log('Inicio del sistema de gestion de transporte');
 
+    require_once './autoload.php';
     require_once './config/app.php';
     require_once './src/helpers/session_start.php';
+
+    use src\controllers\viewsController;
 
     if(isset($_GET['views'])){
         $url = explode("/", $_GET['views']);
     } else {
         $url = ['login'];
     }
+
+    $viewsController = new viewsController();
+    $view = $viewsController->getViewsController($url[0]);
+
 ?>
 
 <!DOCTYPE html>
@@ -22,9 +29,17 @@
     <?php require_once "./src/helpers/includes/header.php"; ?>
 </head>
 <body class="app sidebar-mini">
-    
-    <?php require_once "./src/helpers/includes/nav.php"; ?>
+    <?php
 
-    <?php require_once "./src/helpers/includes/script.php"; ?>
+        if ($view == "./src/views/login/login.php" || $view == "./src/views/errors/404.php"){
+            require_once $view;
+        } else {
+            require_once $view;
+        }
+
+        require_once "./src/helpers/includes/nav.php";
+        require_once "./src/helpers/includes/script.php"; 
+    
+    ?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,39 +1,50 @@
-<?php
-    require_once 'config/app.php';
-?>
-
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="<?= URL; ?>public/img/clover-logo-tab.ico">
-    <link rel="stylesheet" type="text/css" href="<?= URL; ?>public/css/main.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?= URL; ?>public/css/libs/bootstrap.min.css">
-    <title><?= APP_NAME; ?></title>
-</head>
-
-<body>
-    <section class="login-content">
-        <div class="logo">
-            <img class="mb-0" src="<?= URL; ?>public/img/logo-clover-signin.png" style="width: 150px; height: 70px;">
+    <head>
+        <meta charset="<?= CHARSET; ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="icon" href="<?= URL; ?>public/img/clover-logo-tab.ico">
+        <title><?= APP_NAME; ?></title>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" type="text/css" href="<?= URL; ?>public/css/libs/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="<?= URL; ?>public/css/login.css">
+    </head>
+    <body>
+        <div class="container" id="container">
+            <div class="form-container sign-in-container">
+                <form class="row gx-3 allign-items-center">
+                    <div class="col-auto">
+                        <h1>Inicio de sesión</h1>
+                    </div>
+                    <div class="col-auto">
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="bi bi-person-fill"></i></div>
+                            <input type="text" class="form-control" id="user" name="user" placeholder="Usuario" required>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-auto">
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="bi bi-key-fill"></i></div>
+                            <input type="password" class="form-control" name="pass" id="pass" placeholder="Contraseña" required>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <button type="button" class="btn btn-success" id="log">Ingresar</button>
+                    </div>
+                </form>
+            </div>
+            <div class="overlay-container" id="overlayCon">
+                <div class="overlay">
+                    <div class="overlay-panel overlay-right">
+                        <img src="<?= URL; ?>public/img/dashboard.webp" alt="Transporte clover">
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="login-box mt-0">
-            <form class="login-form" action="">
-                <h3 class="login-head"><i class="bi bi-door-open me-2"></i>Iniciar sesion</h3>
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control fw-light" id="username" name="username" placeholder="ingresa tu usuario"/>
-                    <label for="username">Usuario</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="password" name="password" placeholder="ingresa tu contraseña">
-                    <label for="password">Contraseña</label>
-                </div>
-                <div class="d-grid">
-                    <button class="btn btn-success rounded-3"></i>Ingresar</button>
-                </div>
-            </form>
-        </div>
-    </section>
-</body>
+    </body>
+    <footer>
+        <script src="<?= URL; ?>public/js/libs/jquery-3.7.1.min.js"></script>
+        <script src="<?= URL; ?>public/js/libs/bootstrap.bundle.min.js"></script>
+    </footer>
+</html>
