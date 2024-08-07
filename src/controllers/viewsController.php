5e576c81eca3d60a391225fd2de1122f1cfdbcf2
jesus <jesusjsg03@gmail.com>
2024-08-06 22:06:32 -0400

    index establecido para la variable views

--- src/controllers/viewsController.php
+++ src/controllers/viewsController.php
@@ -3,10 +3,10 @@
     namespace src\controllers;
     use src\models\viewsModel;
 
-    class viewsController extends viewsModel{
+    class viewsController extends viewsModel {
         
-        public function getViewsController($folder,$view){
-            if(!empty($folder) && !empty($view)){
+        public function getViewsController($folder, $view = "index") {  
+            if (!empty($folder) && !empty($view)) {
                 return $this->getViewsModel($folder, $view);
             } else {
                 return "./src/views/auth/login.php";
