37e4a39409431dc6ce498db0ef773b8e9c3961c0
jesus <jesusjsg03@gmail.com>
2024-08-07 19:58:04 -0400

    vista usuario finalizada

--- src/views/usuario/index.php
+++ src/views/usuario/index.php
@@ -1,1 +1,37 @@
-vista usuario
+<main class="app-content">
+    <div class="app-title">
+        <div>
+            <h1><i class="bi bi-people-fill"></i>Mantenimiento de los usuarios</h1>
+            <br>
+            <a class="btn btn-success" href="#">Agregar usuario</a>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+            <li class="breadcrumb-item"><i class="bi bi-house-door-fill fs-6"></i></li>
+            <li class="breadcrumb-item"><a class="text-decoration-none" href="<?= URL; ?>home">Inicio</a></li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="tile">
+                <div class="tile-body">
+                    <h6 class="fw-bold">Listado de los usuairos registrados</h6>
+                </div>
+                <br>
+                <div class="table-responsive">
+                    <table class="table row-border display compact table-hover" id="table-usuario" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>ID usuario</th>
+                                <th>Nombre y apellido</th>
+                                <th>Usuario</th>
+                                <th>Contraseña</th>
+                                <th>Rol</th>
+                                <th>Opciones</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
