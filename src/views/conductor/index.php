76944993984a89669754d8a0b1cbabb8e50aa9b9
jesus <jesusjsg03@gmail.com>
2024-08-07 19:56:07 -0400

    vista del conductor finalizada

--- src/views/conductor/index.php
+++ src/views/conductor/index.php
@@ -1,1 +1,44 @@
-me llamo jesus este es el dashboard
+<main class="app-content">
+    <div class="app-title">
+        <div>
+            <h1><i class="bi bi-people-fill"></i>Mantenimiento de los conductores</h1>
+            <br>
+            <a class="btn btn-success" href="#">Agregar conductor</a>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+            <li class="breadcrumb-item"><i class="bi bi-house-door-fill fs-6"></i></li>
+            <li class="breadcrumb-item"><a class="text-decoration-none" href="<?= URL; ?>home">Inicio</a></li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="tile">
+                <div class="tile-body">
+                    <h6 class="fw-bold">Listado de los conductores registrados</h6>
+                </div>
+                <br>
+                <div class="table-responsive">
+                    <table class="table row-border display compact table-hover" id="table-conductor" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>Ficha</th>
+                                <th>Conductor</th>
+                                <th>Cédula</th>
+                                <th>Teléfono</th>
+                                <th>Vehículo</th>
+                                <th>FV Cédula</th>
+                                <th>FV Licencia</th>
+                                <th>FV CM</th>
+                                <th>FV MPPPS</th>
+                                <th>FV Saberes</th>
+                                <th>FV MS</th>
+                                <th>FV Alimento</th>
+                                <th>Opciones</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
