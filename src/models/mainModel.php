e3553fdcda32b28c92990833388d201ac44e9d64
Jesus Santana <pasante-ti@cloverve.com>
2024-08-08 15:20:40 -0400

    feat: modelo principal para la seguridad
    Se creo la funcion de la conexion a la db. Validadcion de datos y prevencion a inyecciones SQL

--- src/models/mainModel.php
+++ src/models/mainModel.php
@@ -1,4 +1,5 @@
 <?php
+
     namespace src\models;
     use \PDO;
 
@@ -13,9 +14,46 @@
         private $user = USER;
         private $pass = PASSWORD;
 
-        protected function conection(){
+        protected function conection(){ //Funcion para la conexión a la base de datos
             $connection = new PDO('mysql:host='.$this->host.'dbname='.$this->db, $this->user, $this->pass);
             $connection->exec("SET CHARACTER SET utf8");
             return $connection;
+        }
+
+        protected function executeQuery($query){ // Función para ejecutar las consultas preparadas
+            $sql = $this->conection()->prepare($query); 
+            $sql->execute();
+            return $sql;
+        }
+
+        // Uso de funciones para evitar la inyección SQL y validar los datos
+        public function cleanString($string){
+
+            $words = [
+                "<script>","</script>","<script src",
+                "<script type=","SELECT * FROM","DELETE FROM",
+                "INSERT INTO","DROP TABLE","DROP DATABASE",
+                "TRUNCATE TABLE","SHOW TABLES","SHOW DATABASES",
+                "<?php","?>","--","^","<",">","==","=",";","::"
+            ];
+            
+            $string = trim($string);
+            $string = stripslashes($string);
+
+            foreach($words as $word){
+                $string = str_ireplace($word, "", $string);
+            };
+
+            $string = trim($string);
+            $string = stripslashes($string);
+            return $string;
+        }
+
+        protected function verifyData($filter, $string){
+            if(preg_match("/^".$filter."$/", $string)){
+                return false;
+            }else {
+                return true;
+            }
         }
     }
