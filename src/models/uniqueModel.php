9813c24980ca0427cdeae28c9948e0498b39d795
Jesus Santana <pasante-ti@cloverve.com>
2024-08-08 15:22:51 -0400

    feat: modelo principal de una primary key

---
+++ src/models/uniqueModel.php
@@ -0,0 +1,93 @@
+<?php
+
+    namespace src\models;
+
+    // Clase para el modelo de las tablas que cuenten con solo una llave primaria
+    class uniqueModel extends mainModel{
+
+        protected function saveData($table, $data){
+
+            //Se recorren los nombres de los campos de la tabla
+            $query = "INSERT INTO $table (";
+            $count = 0;
+
+            foreach($data as $key){
+                if($count >= 1){
+                    $query.=",";
+                }
+                $query.=$key["field_name"];
+                $count++;
+            }
+
+            //Se recorren los valores de los campos de la tabla
+            $query.=") VALUES(";
+            $count = 0;
+            foreach($data as $key){
+                if($count >= 1){
+                    $query.=",";
+                }
+                $query.=$key["field_marker"];
+                $count++;
+            }
+
+            $query.=")";
+            $sql = $this->conection()->prepare($query);
+            foreach($data as $key){
+                $sql->bindParam($key["field_marker"], $key["field_value"]);
+            }
+
+            $sql->execute();
+            return $sql;
+        }
+
+        public function selectData($type,$table,$field,$id){
+
+            $type = $this->cleanString($type);
+            $table = $this->cleanString($table);
+            $field = $this->cleanString($field);
+            $id = $this->cleanString($id);
+
+            if($type == "Primary"){
+                $sql = $this->conection()->prepare("SELECT * FROM $table WHERE $field=:ID");
+                $sql->bindParam(":ID",$id);
+            }elseif($type == "Normal"){
+                $sql = $this->conection()->prepare("SELECT $field FROM $table");
+            }
+            $sql->execute();
+            return $sql;
+        }
+
+        protected function updateData($table,$data,$condition){
+            
+            $query = "UPDATE $table SET ";
+            $count = 0;
+
+            foreach ($data as $key){
+                if($count >= 1){
+                    $query.=",";
+                }
+                $query.=$key["field_name"]."=".$key["field_marker"];
+                $count++;
+            }
+
+            $query.=" WHERE ".$condition["condition_field"]."=".$condition["condition_marker"];
+            $sql = $this->conection()->prepare($query);
+
+            foreach($data as $key){
+                $sql->bindParam($key["field_marker"], $key["field_value"]);
+            }
+
+            $sql->bindParam($condition["condition_marker"], $condition["condition_value"]);
+            $sql->execute();
+            return $sql;
+        }
+        
+        protected function deleteData($table,$field,$id){
+
+            $sql = $this->conection()->prepare("DELETE FROM $table WHERE $field=:id");
+            $sql->bindParam(":id",$id);
+            $sql->execute();
+            return $sql;
+        }
+
+    }
