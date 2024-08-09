0e15b63de98d48a76c9748cff3c8f29c2d43a3e6
Jesus Santana <pasante-ti@cloverve.com>
2024-08-09 09:37:18 -0400

    fix: se establecio nuevas rutas para la edicion

--- src/models/viewsModel.php
+++ src/models/viewsModel.php
@@ -7,7 +7,7 @@ class viewsModel {
     protected function getViewsModel($folder, $view) {
 
         $folderList = ["conductor", "viaje", "general", "ruta", "usuario", "vehiculo", "home"];
-        $viewList = ["dashboard", "form", "index"]; // Se agrega el index para el inicio de las vistas
+        $viewList = ["dashboard", "form", "index", "edit"]; // Se agrega el index para el inicio de las vistas
         
         if (in_array($folder, $folderList) && in_array($view, $viewList)) {
             $filePath = "./src/views/{$folder}/{$view}.php";
