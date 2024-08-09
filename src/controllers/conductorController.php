95f90e1781eb2b619b062390b6f5592a44308347
jesus <jesusjsg03@gmail.com>
2024-08-08 21:34:02 -0400

    feat: creacion de los controladores

---
+++ src/controllers/conductorController.php
@@ -0,0 +1,6 @@
+<?php
+
+    namespace src\controllers;
+    use src\models\uniqueModel;
+
+    class 
---
+++ src/controllers/vehiculoController.php
@@ -0,0 +1,9 @@
+<?php
+
+    namespace src\controllers;
+    use src\models\uniqueModel;
+
+    class vehiculoController extends uniqueModel{
+
+        
+    }
