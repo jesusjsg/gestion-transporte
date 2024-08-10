f26bb542100971e51563ecdd2f849a0e4ecbcf67
jesus <jesusjsg03@gmail.com>
2024-08-09 23:40:58 -0400

    fix: cambios en la url de los estilos

--- src/helpers/includes/header.php
+++ src/helpers/includes/header.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="<?= CHARSET; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="<?= URL; ?>public/img/clover-logo-tab.ico">
+    <link rel="icon" type="image/x-icon" href="<?= URL; ?>public/img/main/clover-logo-tab.ico">
     <title><?= APP_NAME; ?></title>
     <link rel="stylesheet" href="<?= URL; ?>public/css/main.css">
     <link rel="stylesheet" type="text/css" href="<?= URL; ?>public/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?= URL; ?>public/css/libs/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= URL; ?>public/css/libs/sweetalert2.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= URL; ?>public/css/libs/sweetalert2.min.css">
 </head>
