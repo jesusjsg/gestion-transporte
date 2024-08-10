8fd6753f395a47d021fa9cc8a932e97f9bcd9612
jesus <jesusjsg03@gmail.com>
2024-08-09 23:39:39 -0400

    feat: nuevas vista para el error 403
    fix: cambios de las url en el login

--- src/views/auth/login.php
+++ src/views/auth/login.php
@@ -4,31 +4,32 @@
 <head>
     <meta charset="<?= CHARSET; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="<?= URL; ?>public/img/clover-logo-tab.ico">
+    <link rel="icon" type="image/x-icon" href="<?= URL; ?>public/img/main/clover-logo-tab.ico">
     <title><?= APP_NAME; ?></title>
     <link rel="stylesheet" href="<?= URL; ?>public/css/main.css">
+    <link rel="stylesheet" type="text/css" href="<?= URL; ?>public/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?= URL; ?>public/css/libs/bootstrap.min.css">
     <link rel="stylesheet" href="<?= URL; ?>public/css/libs/sweetalert2.min.css" rel="stylesheet">
 </head>
 <body>
     <main>
-        <section class="login-content">
-          <!-- <div class="logo">
-            <h1>Vali</h1>
-          </div> -->
-            <div class="login-box">
+        <section class="login-content shadow-sm">
+            <div class="logo mb-1" style="width: 15%;">
+                <img src="<?= URL; ?>public/img/login/login-clover.webp" alt="login image">
+            </div>
+            <div class="login-box mt-0">
                 <form class="login-form" action="" method="POST">
                     <h3 class="login-head"><i class="bi bi-person me-2"></i>Iniciar sesion</h3>
                     <div class="mb-3">
                         <label class="form-label">Usuario</label>
                         <input type="text" class="form-control" name="username" placeholder="Usuario" autofocus />
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-4">
                         <label class="form-label">Contraseña</label>
-                        <input type="text" class="form-control" name="pass" placeholder="Contraseña" />
+                        <input type="password" class="form-control" name="pass" placeholder="Contraseña" />
                     </div>
-                    <div class="mb-3 btn-container d-grid">
+                    <div class="mb-2 btn-container d-grid">
                         <button type="submit" class="btn btn-success">INGRESAR</button>
                     </div>
                 </form>
---
+++ src/views/errors/403.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Error 403</title>
+    <link rel="stylesheet" href="<?= URL; ?>public/css/libs/bootstrap.min.css" />
+    <style>
+        body, html {
+            height: 100%;
+        }
+    </style>
+</head>
+
+<body class="d-flex justify-content-center align-items-center">
+    <div class="col-md-12 text-center">
+        <h1 class="fw-bold">403</h1>
+        <h2>Sin Autorización</h2>
+        <p class="opacity-75">No tienes permisos para acceder a la vista.</p>
+        <a class="btn btn-secondary" href="javascript:window.history.back();">Volver</a>
+    </div>
+</body>
+
+</html>
