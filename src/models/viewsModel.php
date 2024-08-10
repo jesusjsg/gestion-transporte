4cea064f3e0b95a193458f5775564b319b2560f7
jesus <jesusjsg03@gmail.com>
2024-08-09 23:37:04 -0400

    fix: se agregaron los directorios restantes en el folderlist

--- src/models/viewsModel.php
+++ src/models/viewsModel.php
@@ -6,8 +6,8 @@ class viewsModel {
 
     protected function getViewsModel($folder, $view) {
 
-        $folderList = ["conductor", "viaje", "general", "ruta", "usuario", "vehiculo", "home"];
-        $viewList = ["dashboard", "form", "index", "edit"]; // Se agrega el index para el inicio de las vistas
+        $folderList = ["conductor", "viaje", "general", "ruta", "usuario", "vehiculo", "home", "auth", "errors"];
+        $viewList = ["dashboard", "form", "index", "edit", "closeSesion"]; // Se agrega el index para el inicio de las vistas
         
         if (in_array($folder, $folderList) && in_array($view, $viewList)) {
             $filePath = "./src/views/{$folder}/{$view}.php";
