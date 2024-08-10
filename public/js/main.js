dc789accddd64a40620c2459b9a342ac00cd883e
jesus <jesusjsg03@gmail.com>
2024-08-09 23:11:37 -0400

    feat: nuevo archivo main
    se encuentra la funcion para las fechas del app-nav

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+const date = $('#date');
+
+function main(){
+    dateZone()
+}
+
+function dateZone(){
+    const now = dayjs().locale('es')
+    const format = 'DD/MM/YYYY'
+    const nowFormat = now.format(format)
+
+    date.html(nowFormat)
+}
+
+document.addEventListener('DOMContentLoaded', main)
