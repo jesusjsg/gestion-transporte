8334e34d34202ec9fe66eee2640f5267e8827fa2
jesus <jesusjsg03@gmail.com>
2024-08-12 00:55:28 -0400

    perf: se agrego un valor de ejemplo en el select

--- src/views/usuario/form.php
+++ src/views/usuario/form.php
@@ -16,7 +16,7 @@
                     <button type="submit" class="btn btn-success">Guardar <i class="bi bi-floppy"></i></button>
                 </div>
                 <div class="tile-footer"></div>
-                <input type="hidden" class="form-control" name="model_user" value="register" />
+                <input type="hidden" name="model_user" value="register" />
                 <div class="col-md-3">
                     <label for="fullname" class="form-label">Nombre y apellido</label>
                     <input type="text" class="form-control" name="fullname" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,255}" maxlength="255"/>
@@ -27,16 +27,17 @@
                 </div>
                 <div class="col-md-2">
                     <label for="password" class="form-label">Contraseña</label>
-                    <input type="password" class="form-control" name="password" pattern="[a-zA-Z0-9$@.\-]{7,100}" maxlength="100" />
+                    <input type="password" class="form-control" name="password" pattern="[a-zA-Z0-9$@.\-]{6,100}" maxlength="100" />
                 </div>
                 <div class="col-md-2">
                     <label for="valid-password" class="form-label">Repetir contraseña</label>
-                    <input type="password" class="form-control" name="valid-password" pattern="[a-zA-Z0-9$@.\-]{7,100}" maxlength="100" />
+                    <input type="password" class="form-control" name="valid-password" pattern="[a-zA-Z0-9$@.\-]{6,100}" maxlength="100" />
                 </div>
                 <div class="col-md-2">
                     <label for="id-rol" class="form-label">Rol del usuario</label>
                     <select class="form-select" name="id-rol">
                         <option selected disabled>Rol del usuario</option>
+                        <option value="1">Administrador</option>
                     </select>
                 </div>
                 <div class="tile-footer"></div>
