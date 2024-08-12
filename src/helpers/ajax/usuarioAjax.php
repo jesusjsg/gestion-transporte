91d6be01467c2940e9d43626c585592157229b3a
jesus <jesusjsg03@gmail.com>
2024-08-11 21:37:35 -0400

    feat: creacion del ajax del usuario
    este archivo se encargara de validar las solicitudes que se realicen del formulario

---
+++ src/helpers/ajax/usuarioAjax.php
@@ -0,0 +1,19 @@
+<?php
+
+    require_once '../../../config/app.php';
+    require_once '../session_start.php';
+    require_once '../../../autoload.php';
+
+    use src\controllers\usuarioController;
+
+    if(isset($_POST['model_user'])){
+
+        $user = new usuarioController();
+
+        if($_POST['model_user'] == 'register'){
+            $user->registerUser();
+        }
+    }else{
+        session_destroy();
+        header('Location: '. URL);
+    }
