f9dd9b89cf3ca02a8e6a66f00c686090671ed2a3
jesus <jesusjsg03@gmail.com>
2024-08-11 21:33:29 -0400

    perf: se agregaron mas rutas en las carpetas y las vistas

--- src/controllers/viewsController.php
+++ src/controllers/viewsController.php
@@ -5,11 +5,13 @@
 
     class viewsController extends viewsModel {
         
-        public function getViewsController($folder, $view = "index") {  
-            if (!empty($folder) && !empty($view)) {
-                return $this->getViewsModel($folder, $view);
+        public function getViewsController($folder, $view) {
+            error_log('Carpeta: ' . $folder. ', Vista: ' . $view);
+            $viewPath = "./src/views/$folder/$view.php";
+            if (is_file($viewPath)) {
+                return $viewPath;
             } else {
-                return "./src/views/auth/login.php";
+                return "./src/views/errors/404.php";
             }
         }
     }
