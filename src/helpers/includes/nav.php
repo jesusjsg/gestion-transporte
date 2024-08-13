2218f281e390882774efc6922f1c088c77677f0e
Jesus Santana <pasante-ti@cloverve.com>
2024-08-13 13:58:40 -0400

    perf: cambio de color de los iconos

--- src/helpers/includes/nav.php
+++ src/helpers/includes/nav.php
@@ -3,29 +3,30 @@
         <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="hide sidebar"></a>
         <ul class="app-nav">
             <div class="app-nav__item user-select-none">
-                <i class="bi bi-calendar me-2"></i><span id="date"></span>
+                <i class="bi bi-person-fill me-2"></i><span></span>
+                <i class="bi bi-calendar me-2"></i><span class="me-2" id="date"></span>
             </div>
         </ul>
     </header>
     <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
     <aside class="app-sidebar">
         <ul class="app-menu"> 
-            <li><a class="app-menu__item" href="<?= URL; ?>home/"><i class="app-menu__icon bi bi-house"></i>
+            <li><a class="app-menu__item" href="<?= URL; ?>home/"><i class="app-menu__icon bi bi-house-fill"></i>
                 <span class="app-menu__label">Inicio</span></a></li>
 
-            <li><a class="app-menu__item" href="<?= URL; ?>viaje/"><i class="app-menu__icon bi bi-cursor"></i>
+            <li><a class="app-menu__item" href="<?= URL; ?>viaje/"><i class="app-menu__icon bi bi-cursor-fill"></i>
                 <span class="app-menu__label">Viajes</span></a></li>
 
-            <li><a class="app-menu__item" href="<?= URL; ?>conductor/"><i class="app-menu__icon bi bi-people"></i>
+            <li><a class="app-menu__item" href="<?= URL; ?>conductor/"><i class="app-menu__icon bi bi-people-fill"></i>
                 <span class="app-menu__label">Conductores</span></a></li>
 
-            <li><a class="app-menu__item" href="<?= URL; ?>ruta/"><i class="app-menu__icon bi bi-geo-alt"></i>
+            <li><a class="app-menu__item" href="<?= URL; ?>ruta/"><i class="app-menu__icon bi bi-geo-alt-fill"></i>
                 <span class="app-menu__label">Rutas</span></a></li>
 
-            <li><a class="app-menu__item" href="<?= URL; ?>vehiculo/"><i class="app-menu__icon bi bi-car-front"></i>
+            <li><a class="app-menu__item" href="<?= URL; ?>vehiculo/"><i class="app-menu__icon bi bi-car-front-fill"></i>
                 <span class="app-menu__label">Vehículos</span></a></li>
 
-            <li><a class="app-menu__item" href="<?= URL; ?>general/"><i class="app-menu__icon bi bi-info-circle"></i>
+            <li><a class="app-menu__item" href="<?= URL; ?>general/"><i class="app-menu__icon bi bi-info-circle-fill"></i>
                 <span class="app-menu__label">General</span></a></li>
             
             <li><a class="app-menu__item" href="<?= URL; ?>nomina/"><i class="app-menu__icon bi bi-file-text"></i>
@@ -38,11 +39,11 @@
                 </ul>
             </li>
 
-            <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon bi bi-gear"></i>
+            <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon bi bi-gear-fill"></i>
                 <span class="app-menu__label">Configuración</span><i class="treeview-indicator bi bi-chevron-right"></i></a>
                 <ul class="treeview-menu">
-                    <li><a class="treeview-item" href="<?= URL; ?>usuario/"><i class="icon bi bi-person"></i>Usuarios</a></li>
-                    <li><a class="treeview-item" href="<?= URL; ?>permisos/"><i class="icon bi bi-lock"></i>Permisos</a></li>
+                    <li><a class="treeview-item" href="<?= URL; ?>usuario/"><i class="icon bi bi-person-fill"></i>Usuarios</a></li>
+                    <li><a class="treeview-item" href="<?= URL; ?>permisos/"><i class="icon bi bi-lock-fill"></i>Permisos</a></li>
                 </ul>
             </li>
         </ul>
