0f9cb295eaf0c293b97ebc84204e8325ca77de32
Jesus Santana <pasante-ti@cloverve.com>
2024-08-13 10:44:40 -0400

    feat: nueva clase del autocompletado

---
+++ src/helpers/components/autocomplete.php
@@ -0,0 +1,21 @@
+<?php 
+    
+    namespace src\helpers\components;
+
+use PDO;
+use src\models\mainModel;
+
+    class Autocomplete extends mainModel{
+
+        public function autocompleteSelect($idRegistro){
+            $fillOptions = $this->executeQuery("SELECT id_entidad, descripcion1 FROM general WHERE id_registro = '$idRegistro' AND id_entidad > 0");
+            $options = [];
+
+            if($fillOptions->rowCount()>0){
+                while($row = $fillOptions->fetch(PDO::FETCH_ASSOC)){
+                    $options[] = $row;
+                }
+            }
+            return $options;
+        }
+    }
