c9e93b7aae50838eb39851595b8013cd5f45018c
jesus <jesusjsg@proton.me>
2024-08-19 00:17:53 -0400

    perf: se eliminaron las doble validaciones en el viewController

--- src/controllers/viewsController.php
+++ src/controllers/viewsController.php
@@ -1,17 +1,13 @@
 <?php
 
     namespace src\controllers;
+
     use src\models\viewsModel;
 
     class viewsController extends viewsModel {
         
         public function getViewsController($folder, $view) {
-            error_log('Carpeta: ' . $folder. ', Vista: ' . $view);
-            $viewPath = "./src/views/$folder/$view.php";
-            if (is_file($viewPath)) {
-                return $viewPath;
-            } else {
-                return "./src/views/errors/404.php";
-            }
+            error_log('Carpeta: ' . $folder . ', Vista: ' . $view);
+            return $this->getViewsModel($folder, $view);
         }
     }
