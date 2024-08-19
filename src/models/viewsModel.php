9dcc9b53220cdba0d3b089c693979e022299afd8
jesus <jesusjsg@proton.me>
2024-08-19 00:19:17 -0400

    perf: se agrego el protected a los array y a las funciones

--- src/models/viewsModel.php
+++ src/models/viewsModel.php
@@ -1,26 +1,26 @@
 <?php
 
-namespace src\models;
+    namespace src\models;
 
-class viewsModel {
+    class viewsModel {
+        protected $folderList = ["conductor", "viaje", "general", "ruta", "usuario", "vehiculo", "home", "auth", "errors", "nomina"];
+        protected $viewList = ["dashboard", "form", "index", "edit", 'login', "closeSesion"];
 
-    protected function getViewsModel($folder, $view) {
-
-        $folderList = ["conductor", "viaje", "general", "ruta", "usuario", "vehiculo", "home", "auth", "errors", "nomina"];
-        $viewList = ["dashboard", "form", "index", "edit", "closeSesion"]; // Se agrega el index para el inicio de las vistas
-        
-        if (in_array($folder, $folderList) && in_array($view, $viewList)) {
-            $filePath = "./src/views/{$folder}/{$view}.php";
-            if (is_file($filePath)) {
-                return $filePath;
+        protected function getViewsModel($folder, $view) {
+            if (in_array($folder, $this->folderList)) {
+                
+                if (empty($view) || !in_array($view, $this->viewList)) {
+                    $view = 'index';
+                }
+                return $this->checkFile("./src/views/{$folder}/{$view}.php");
+            } elseif ($folder == "auth" && $view == "login") {
+                return "./src/views/{$folder}/{$view}.php";
             } else {
                 return "./src/views/errors/404.php";
             }
-        } elseif ($folder == "auth" && $view == "login") {
-            return "./src/views/{$folder}/{$view}.php";
-        } else {
-            return "./src/views/errors/404.php";
         }
 
+        protected function checkFile($filePath) {
+            return is_file($filePath) ? $filePath : "./src/views/errors/404.php";
+        }
     }
-}
