e0f3d79ad00ed34fed2a75379e78710f34b0576f
jesus <jesusjsg@proton.me>
2024-08-20 23:54:23 -0400

    fix: se soluciono el problema de las peticiones ajax

--- src/helpers/ajax/usuarioAjax.php
+++ src/helpers/ajax/usuarioAjax.php
@@ -1,9 +1,5 @@
 <?php
 
-    require_once '../../../config/app.php';
-    require_once '../session_start.php';
-    require_once '../../../autoload.php';
-
     use src\controllers\usuarioController;
 
     if(isset($_POST['model_user'])){
