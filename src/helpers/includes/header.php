c03b64866777f3c505f7483e98957c56e5eec281
jesus santana <jesusjsg@proton.me>
2024-08-22 10:06:36 -0400

    perf: nuevos estilos del datatables

--- src/helpers/includes/header.php
+++ src/helpers/includes/header.php
@@ -9,5 +9,6 @@
     <link rel="stylesheet" type="text/css" href="<?= URL; ?>public/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?= URL; ?>public/css/libs/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= URL; ?>public/css/libs/datatables.min.css">
     <link rel="stylesheet" href="<?= URL; ?>public/css/libs/sweetalert2.min.css">
 </head>
