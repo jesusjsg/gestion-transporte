b8a6715da02fa862e64c204770d17c2120540aaa
jesus santana <jesusjsg@proton.me>
2024-08-23 16:31:33 -0400

    feat: script para el conductor

---
+++ public/js/functions/conductor.js
@@ -0,0 +1,25 @@
+import { getDatatable } from "../components/datatable.js";
+
+const tableUrl = 'http://localhost/gestion-transporte/ajax/conductor?action=load_conductores'
+const autocompleteUrl = ''
+
+function main(){
+    getDatatable(tableUrl, [
+        {'data': 'id_conductor'},
+        {'data': 'nombre_conductor'},
+        {'data': 'cedula_conductor'},
+        {'data': 'telefono_conductor'},
+        {'data': 'id_vehiculo'},
+        {'data': 'vencimiento_cedula'},
+        {'data': 'vencimiento_licencia'},
+        {'data': 'vencimiento_certificadoMedico'},
+        {'data': 'vencimiento_mppps'},
+        {'data': 'vencimiento_saberes'},
+        {'data': 'vencimiento_manejoSeguro'},
+        {'data': 'vencimiento_alimento'},
+        {'data': 'tipo_nomina'}
+    ])
+
+}
+
+document.addEventListener('DOMContentLoaded', main())
