7c6e43a30afa6c5b22570a8d558ebdbd3df5da8b
jesus santana <jesusjsg@proton.me>
2024-08-23 15:11:59 -0400

    perf: se eliminaron deadlines

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     ini_set('ignore_repeated_errors', false);
     ini_set('display_errors', true); // Cambiar a false en producción
     ini_set('log_errors', true);
-    ini_set('error_log', '/laragon/www/gestion-transporte/php-errors.log'); //Para el xampp instalado en linux
+    ini_set('error_log', '/laragon/www/gestion-transporte/php-errors.log');
     error_log('Inicio del sistema de gestión de transporte');
 
-    require_once './autoload.php';
-    require_once './vendor/autoload.php';
-    require_once './config/app.php';
-    require_once './src/helpers/session_start.php';
+    require_once 'autoload.php';
+    require_once 'vendor/autoload.php';
+    require_once 'config/app.php';
+    require_once 'src/helpers/session_start.php';
 
     use src\controllers\viewsController;
     use src\controllers\loginController;
@@ -22,6 +22,7 @@
         $url = explode("/", $_GET['views']);
         $folder = $url[0] ?? 'home';
         $view = $url[1] ?? 'index';
+
     }else{
         $folder = 'auth';
         $view = 'login'; 
@@ -39,3 +40,4 @@
     if ($viewPath != "./src/views/auth/login.php" && $viewPath != "./src/views/errors/404.php") {
         require_once "./src/helpers/includes/script.php";
     }
+?>
