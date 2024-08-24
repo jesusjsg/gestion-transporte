f9aa929b8c5f677d2b76a167bf5111801c0077d3
jesus <jesusjsg@proton.me>
2024-08-24 08:33:05 -0400

    feat: nueva funcion autocompleteInput

--- src/helpers/components/autocomplete.php
+++ src/helpers/components/autocomplete.php
@@ -19,4 +19,22 @@
             }
             return $options;
         }
+
+        public function autocompletePlaca($term){
+            $term = '%' . $term . '%';
+
+            $fillOptions = $this->executeQuery(
+                "SELECT id_vehiculo FROM vehiculo WHERE id_vehiculo LIKE :term ORDER BY id_vehiculo ASC"
+            );
+            $fillOptions->bindParam(':term', $term, PDO::PARAM_STR);
+            
+            $data = [];
+
+            if($fillOptions->rowCount()>0){
+                while($row = $fillOptions->fetch(PDO::FETCH_ASSOC)){
+                    $data[] = $row['id_vehiculo'];
+                }
+            }
+            return json_encode($data);
+        }
     }
