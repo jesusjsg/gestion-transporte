e0d97ecbe3e662ed5e1ffbee43bf3861d32d241d
jesus <jesusjsg@proton.me>
2024-08-24 08:32:22 -0400

    feat: nuevo ajax para el autocompletado

--- src/helpers/ajax/conductorAjax.php
+++ src/helpers/ajax/conductorAjax.php
@@ -1,6 +1,7 @@
 <?php
 
     use src\controllers\conductorController;
+    use src\helpers\components\Autocomplete;
 
     if(isset($_POST['model_conductor'])){
         $conductor = new conductorController;
@@ -12,7 +13,11 @@
     }elseif(isset($_GET['action']) && $_GET['action'] == 'load_conductores'){
         $conductor = new conductorController;
         echo $conductor->tableConductor();
-        
+
+    }elseif(isset($_GET['action']) && $_GET['action'] == 'get_placa' && isset($_GET['term'])){
+        $placa = new Autocomplete;
+        echo $placa->autocompletePlaca($_GET['term']);
+
     }else{
         session_destroy();
         header('Location: '. URL);
