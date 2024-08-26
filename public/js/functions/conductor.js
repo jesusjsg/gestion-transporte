3b91381ae928d6a09e84390b57dcb0933efd15ba
jesus <jesusjsg@proton.me>
2024-08-25 21:44:22 -0400

    perf: uso del getPlaca

--- public/js/functions/conductor.js
+++ public/js/functions/conductor.js
@@ -1,7 +1,9 @@
 import { getDatatable } from "../components/datatable.js";
+import { getPlaca } from "../components/autocomplete.js";
 
 const tableUrl = 'http://localhost/gestion-transporte/ajax/conductor?action=load_conductores'
 const autocompleteUrl = 'http://localhost/gestion-transporte/ajax/conductor?action=get_placa'
+const fieldId = '#vehiculo-conductor'
 
 function main(){
     getDatatable(tableUrl, [
@@ -19,6 +21,8 @@ function main(){
         {'data': 'vencimiento_alimento'},
         {'data': 'tipo_nomina'}
     ])
+
+    getPlaca(fieldId, autocompleteUrl)
 }
 
 document.addEventListener('DOMContentLoaded', main)
