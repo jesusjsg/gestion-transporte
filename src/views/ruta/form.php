c5498e8eb9ace7fb87c55619447e18bf8eff5f9a
jesus santana <jesusjsg@proton.me>
2024-08-26 16:43:55 -0400

    perf: modificacion de los campos ocultos

--- src/views/ruta/form.php
+++ src/views/ruta/form.php
@@ -10,23 +10,16 @@
                     <button type="submit" class="btn btn-success btn-sm">Guardar<i class="bi bi-floppy ms-1"></i></button>
                 </div>
                 <div class="tile-footer"></div>
-
-                <input type="hidden" name="model_ruta" value="register" hidden />
-                <input type="hidden" name="codigo-ruta" hidden/>
-                <input type="hidden" name="nombre-ruta" hidden/>
-                <!-- <div class="col-md-2">
-                    <label for="codigo-ruta" class="form-label">Código ruta</label>
-                </div>
-                <div class="col-md-3">
-                    <label for="nombre-ruta" class="form-label">Nombre</label>
-                </div> -->
+                <input type="hidden" name="model_ruta" value="register" />
+                <input type="hidden" id="codigo-origen" />
+                <input type="hidden" id="codigo-destino" />
                 <div class="col-md-4">
                     <label for="origen" class="form-label">Origen</label>
-                    <input type="text" class="form-control autocomplete" name="origen" pattern="[a-zA-Z ]{5,255}" maxlength="255" />
+                    <input type="text" class="form-control" id="origen" pattern="[a-zA-Z ]{5,255}" maxlength="255" />
                 </div>
                 <div class="col-md-4">
                     <label for="destino" class="form-label">Destino</label>
-                    <input type="text" class="form-control autocomplete" name="destino" pattern="[a-zA-Z ]{5,255}" maxlength="255"/>
+                    <input type="text" class="form-control" id="destino" pattern="[a-zA-Z ]{5,255}" maxlength="255"/>
                 </div>
                 <div class="col-md-4">
                     <label for="kilometros" class="form-label">Kilometros</label>
