2e2fd25486e4158ae4bf64293bc4be9327ac0413
jesus santana <jesusjsg@proton.me>
2024-08-28 11:06:44 -0400

    feat: se carga el datatable general

---
+++ public/js/functions/general.js
@@ -0,0 +1,17 @@
+import { getDatatable } from "../components/datatable.js"
+
+const tableUrl = 'http://localhost/gestion-transporte/ajax/general?action=load_general';
+const tableId = document.querySelector('#table-general')
+
+function main(){
+    getDatatable(tableId, tableUrl, [
+        {'data': 'id_registro'},
+        {'data': 'id_entidad'},
+        {'data': 'descripcion1'},
+        {'data': 'descripcion2'},
+        {'data': 'descripcion3'},
+        {'data': 'valor', 'className':'dt-right'}
+    ])
+}
+
+document.addEventListener('DOMContentLoaded', main)
