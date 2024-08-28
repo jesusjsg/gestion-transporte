6c45698174afc073a729df86657b6c76f59b5fbc
jesus santana <jesusjsg@proton.me>
2024-08-28 16:58:19 -0400

    feat: ajax para el formulario y datatable

---
+++ src/helpers/ajax/generalAjax.php
@@ -0,0 +1,21 @@
+<?php
+
+    use src\controllers\generalController;
+
+    if(isset($_POST['model_general'])){
+        $general = new generalController;
+
+        if($_POST['model_general'] == 'register'){
+            echo $general->registerGeneral();
+        }
+
+        if($_POST['model_general'] == 'delete'){
+            echo $general->deleteGeneral();
+        }
+    }elseif(isset($_GET['action']) && $_GET['action'] == 'load_general'){
+        $general = new generalController;
+        echo $general->tableGeneral();
+    }else{
+        session_destroy();
+        header('Location: '. URL);
+    }
