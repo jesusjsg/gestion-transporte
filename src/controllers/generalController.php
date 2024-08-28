1f9a1953187c7db0a4243084317741742b8bcd8d
jesus santana <jesusjsg@proton.me>
2024-08-28 16:53:40 -0400

    feat: table general agregado

--- src/controllers/generalController.php
+++ src/controllers/generalController.php
@@ -2,20 +2,21 @@
 
     namespace src\controllers;
 
-    use src\models\mainModel;
+use PDO;
+use src\models\doubleModel;
 
-    class generalController extends mainModel{
+    class generalController extends doubleModel{
 
-        protected function registerGeneral(){
+        public function registerGeneral(){
 
             $codigoRegistro = intval($this->cleanString($_POST['codigo-registro']));
             $codigoEntidad = intval($this->cleanString($_POST['codigo-entidad']));
-            $primeraDescripcion = $this->cleanString($_POST['primera-descripcion']);
-            $segundaDescripcion = $this->cleanString($_POST['segunda-descripcion']);
-            $terceraDescripcion = $this->cleanString($_POST['tercera-descripcion']);
+            $primeraDescripcion = trim($this->cleanString($_POST['primera-descripcion']));
+            $segundaDescripcion = trim($this->cleanString($_POST['segunda-descripcion']));
+            $terceraDescripcion = trim($this->cleanString($_POST['tercera-descripcion']));
             $valor = $this->cleanString($_POST['valor']);
 
-            if(empty($codigoRegistro) || empty($codigoEntidad)){
+            if($codigoRegistro == '' || $codigoEntidad == ''){
                 $alert = [
                     'type' => 'simple',
                     'icon' => 'error',
@@ -35,7 +36,7 @@
                 return json_encode($alert);
             }
 
-            if($this->verifyData('[a-zA-Z ]{4,255}', $primeraDescripcion) || $this->verifyData('[a-zA-Z ]{4,255}', $segundaDescripcion) || $this->verifyData('[a-zA-Z ]{4,255}', $terceraDescripcion)){
+            if($this->verifyData('[a-zA-Z ]{0,255}', $primeraDescripcion) || $this->verifyData('[a-zA-Z ]{0,255}', $segundaDescripcion) || $this->verifyData('[a-zA-Z ]{0,255}', $terceraDescripcion)){
                 $alert = [
                     'type' => 'simple',
                     'icon' => 'error',
@@ -66,5 +67,76 @@
                 ];
                 return json_encode($alert);
             }
+
+            $registroDataLog = [
+                [
+                    'field_name_database' => 'id_registro',
+                    'field_name_form' => ':registro',
+                    'field_value' => $codigoRegistro
+                ],
+                [
+                    'field_name_database' => 'id_entidad',
+                    'field_name_form' => ':entidad',
+                    'field_value' => $codigoEntidad
+                ],
+                [
+                    'field_name_database' => 'descripcion1',
+                    'field_name_form' => ':descripcion1',
+                    'field_value' => $primeraDescripcion
+                ],
+                [
+                    'field_name_database' => 'descripcion2',
+                    'field_name_form' => ':descripcion2',
+                    'field_value' => $segundaDescripcion
+                ],
+                [
+                    'field_name_database' => 'descripcion3',
+                    'field_name_form' => ':descripcion3',
+                    'field_value' => $terceraDescripcion
+                ]
+            ];
+
+            $saveRegistro = $this->saveData('general', $registroDataLog);
+
+            if($saveRegistro->rowCount() == 1){
+                $alert = [
+                    'type' => 'reload',
+                    'icon' => 'success',
+                    'title' => 'Registro exitoso',
+                    'text' => 'La entidad ha sido registrado correctamente.'
+                ];
+            }else{
+                $alert = [
+                    'type' => 'simple',
+                    'icon' => 'error',
+                    'title' => 'Ocurrió un error',
+                    'text' => 'Hubo un problema al registrar la entidad.'
+                ];
+            }
+            return json_encode($alert);
         }
+
+        public function tableGeneral(){
+
+            $getTableGeneral = $this->executeQuery("SELECT * FROM general");
+            $data = [];
+
+            if($getTableGeneral->rowCount()>0){
+                while($row = $getTableGeneral->fetch(PDO::FETCH_ASSOC)){
+                    foreach($row as $key => $value){
+                        if($value == ''){
+                            $row[$key] = '
+                                <span class="badge text-bg-danger">No definido</span>
+                            ';
+                        }
+                    }
+                    $data[] = $row;
+                }
+            }
+            return json_encode($data);
+        }
+
+        public function updateGeneral(){}
+
+        public function deleteGeneral(){}
     }
