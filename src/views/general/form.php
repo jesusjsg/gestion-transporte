9a99480d8ce505b73cc093234a4fec15ca65c0e5
jesus santana <jesusjsg@proton.me>
2024-08-28 11:10:50 -0400

    feat: valor para registrar el registro

--- src/views/general/form.php
+++ src/views/general/form.php
@@ -4,19 +4,20 @@
     </div>
     <div class="tile">
         <div class="tile-body">
-            <form class="row g-3" action="<?= URL; ?>ajax/general" method="post">
+            <form class="row g-3 form-ajax" action="<?= URL; ?>ajax/general" method="post" autocomplete="off">
                 <div class="d-grid gap-2 d-md-flex justify-content-end">
                     <button type="reset" class="btn btn-primary btn-sm">Limpiar<i class="bi bi-archive ms-1"></i></button>
                     <button type="submit" class="btn btn-success btn-sm">Guardar<i class="bi bi-floppy ms-1"></i></button>
                 </div>
                 <div class="tile-footer"></div>
+                <input type="hidden" name="model_general" value="register" />
                 <div class="col-md-1">
                     <label for="codigo-registro" class="form-label">Registro</label>
-                    <input type="text" class="form-control" name="codigo-registro" pattern="[0-9]{1,255}" />
+                    <input type="text" class="form-control" name="codigo-registro" />
                 </div>
                 <div class="col-md-1">
                     <label for="codigo-entidad" class="form-label">Entidad</label>
-                    <input type="text" class="form-control" name="codigo-entidad" pattern="[0-9]{1,255}"/>
+                    <input type="text" class="form-control" name="codigo-entidad" />
                 </div>
                 <div class="col-md-3">
                     <label for="primera-descripcion" class="form-label">Descripción 1</label>
