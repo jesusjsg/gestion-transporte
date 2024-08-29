0914c3a1a64c5de6435e46b2c5d415c7ac38fff2
jesus santana <jesusjsg@proton.me>
2024-08-29 16:46:20 -0400

    feat: carga dinamica de las filas

--- public/js/components/datatable.js
+++ public/js/components/datatable.js
@@ -1,46 +1,51 @@
-/* 
-Funcion para obtener las tablas de cada vista
-*/
+import { formAjaxHandler } from "../main.js";
 
 export function getDatatable(fieldId, url, columns){
-    $(fieldId).DataTable({
-        "aProcessing": true,
-        "aServerside": true,
-        language: {
-            'processing': 'Procesando...',
-            'lengthMenu': 'Mostrar registros _MENU_',
-            'zeroRecords': 'No se encontraron resultados',
-            'info': 'Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros',
-            'infoEmpty': 'Mostrando registros del 0 al 0 de un total de 0 registros',
-            'infoFiltered': '(filtrado de un total de _MAX_ registros)',
-            'search': 'Buscar:',
-            'loadingRecords': 'Cargando',
-            'paginate': {
-                'first': 'Primero',
-                'last': 'Último',
-                'next': 'Siguiente',
-                'previous': 'Anterior'
-            }
-        },
-        'ajax': {
-            'url': url,
-            'dataSrc':'',
-        },
-        dom: 'Bfrtip',
-        buttons: [
-            {
-                extend: 'excelHtml5',
-                text: 'Excel <i class="bi bi-file-earmark-excel"></i>',
-                className: 'btn btn-success btn-sm',
-                exportOptions: {
-                    coluumns: ':all'
+    $(document).ready(function(){
+        $(fieldId).DataTable({
+            "aProcessing": true,
+            "aServerside": true,
+            language: {
+                'processing': 'Procesando...',
+                'lengthMenu': 'Mostrar registros _MENU_',
+                'zeroRecords': 'No se encontraron resultados',
+                'info': 'Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros',
+                'infoEmpty': 'Mostrando registros del 0 al 0 de un total de 0 registros',
+                'infoFiltered': '(filtrado de un total de _MAX_ registros)',
+                'search': 'Buscar:',
+                'loadingRecords': 'Cargando',
+                'paginate': {
+                    'first': 'Primero',
+                    'last': 'Último',
+                    'next': 'Siguiente',
+                    'previous': 'Anterior'
                 }
-            }
-        ],
-        'columns': columns,
-        'responsive': true,
-        'bDestroy': true,
-        'iDisplayLength': 15,
-        'order': [[0, 'asc']],
+            },
+            createdRow: function(row, data, dataIndex){
+                const form = row.querySelector(".form-ajax")
+                formAjaxHandler(form)
+            },
+            'ajax': {
+                'url': url,
+                'dataSrc':'',
+            },
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'excelHtml5',
+                    text: 'Excel <i class="bi bi-file-earmark-excel"></i>',
+                    className: 'btn btn-success btn-sm',
+                    exportOptions: {
+                        coluumns: ':all'
+                    }
+                }
+            ],
+            'columns': columns,
+            'responsive': true,
+            'bDestroy': true,
+            'iDisplayLength': 15,
+            'order': [[0, 'asc']],
+        })
+
     })
 }
