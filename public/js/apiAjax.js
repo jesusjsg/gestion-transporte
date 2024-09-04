387001a5d293939ef74b365ec65bf9cf0a9fe060
jesus santana <jesusjsg@proton.me>
2024-09-04 16:30:02 -0400

    feat: constantes de las apis

---
+++ public/js/apiAjax.js
@@ -0,0 +1,15 @@
+export const AJAX_TABLES = {
+    viaje: 'http://localhost/gestion-transporte/ajax/viaje?action=load_viaje',
+    conductor: 'http://localhost/gestion-transporte/ajax/conductor?action=load_conductores',
+    general: 'http://localhost/gestion-transporte/ajax/general?action=load_general',
+    vehiculo: 'http://localhost/gestion-transporte/ajax/vehiculo?action=load_vehiculos',
+    ruta: 'http://localhost/gestion-transporte/ajax/ruta?action=load_ruta',
+    usuario: 'http://localhost/gestion-transporte/ajax/usuarios?action=load_users',
+}
+
+export const AJAX_AUTOCOMPLETE = {
+    cliente: 'http://localhost/gestion-transporte/ajax/viaje?action=get_cliente',
+    municipio: 'http://localhost/gestion-transporte/ajax/viaje?action=get_municipio',
+    conductor: 'http://localhost/gestion-transporte/ajax/viaje?action=get_conductor',
+    placaVehiculo: 'http://localhost/gestion-transporte/ajax/viaje?action=get_placa',
+}
