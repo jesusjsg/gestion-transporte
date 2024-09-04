0d5b8ba451e637b63e24a0f2515fe4498bbc8aa8
jesus santana <jesusjsg@proton.me>
2024-09-04 16:34:13 -0400

    feat: fines de semanas calculados

--- public/js/functions/viaje.js
+++ public/js/functions/viaje.js
@@ -1,26 +1,25 @@
+import { alertMessages} from "../alertMessages.js";
+import { AJAX_AUTOCOMPLETE } from "../apiAjax.js";
 import { autocompleteCliente, autocompleteConductor, autocompleteMunicipio } from "../components/autocomplete.js";
 import { getDatatable } from "../components/datatable.js";
+import { getWeekends } from "../getDay.js";
 
 const tableUrl = 'http://localhost/gestion-transporte/ajax/viaje?action=load_viaje'
-const municipioUrl = 'http://localhost/gestion-transporte/ajax/viaje?action=get_municipio'
-const clienteUrl = 'http://localhost/gestion-transporte/ajax/viaje?action=get_cliente'
-const conductorUrl = 'http://localhost/gestion-transporte/ajax/viaje?action=get_conductor'
-const placaUrl = 'http://localhost/gestion-transporte/ajax/viaje?action=get_placa'
-
 const tableId = document.querySelector('#table-viaje')
-
 const cliente = document.querySelector('#cliente')
 const clienteId = document.querySelector('#id-cliente')
-
 const origen = document.querySelector('#origen')
 const origenId = document.querySelector('#id-origen')
-
 const destino = document.querySelector('#destino')
 const destinoId = document.querySelector('#id-destino')
-
 const nombreConductor = document.querySelector('#nombre-conductor')
 const fichaConductor = document.querySelector('#ficha-conductor')
 const placa = document.querySelector('#placa-vehiculo')
+const startDate = document.querySelector('#fecha-inicio')
+const endDate = document.querySelector('#fecha-cierre')
+const countSaturday = document.querySelector('#cantidad-sabados')
+const countSundays = document.querySelector('#cantidad-domingos')
+
 
 function main(){
     getDatatable(tableId, tableUrl, [
@@ -46,10 +45,45 @@ function main(){
         {'data': 'opciones'}
     ])
 
-    autocompleteConductor(nombreConductor, conductorUrl, fichaConductor, placa)
-    autocompleteCliente(cliente, clienteUrl, clienteId)
+    autocompleteConductor(nombreConductor, AJAX_AUTOCOMPLETE.conductor, fichaConductor, placa)
+    autocompleteCliente(cliente, AJAX_AUTOCOMPLETE.cliente, clienteId)
+
+    startDate.addEventListener('change', calculateWeekends)
+    endDate.addEventListener('change', calculateWeekends)
 
 }
+
+function calculateWeekends(){
+    const start = dayjs(startDate.value)
+    const end = dayjs(endDate.value)
+    
+    if(start & end){
+        if(start.isBefore(end) || start.isSame(end)){
+            const weeekends = getWeekends({
+                startDate: start,
+                endDate: end,
+            })
+
+            const totalSaturdays = weeekends.filter(date => dayjs(date).day() === 6).length
+            const totalSundays = weeekends.filter(date => dayjs(date).day() === 0).length
+
+            countSaturday.value = totalSaturdays
+            countSundays.value = totalSundays
+        }else{
+            alertMessages({
+                icon: 'warning',
+                title: 'Ocurrió un error',
+                text: 'La fecha de inicio debe ser anterior a la fecha de cierre.',
+                confirmButtonText: 'Aceptar',
+            })
+        }
+    }
+}
+
+function 
+
+function renderMovements(){}
+
 
 
 
