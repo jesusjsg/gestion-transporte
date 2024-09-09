773039d31f6665f5745d5819826f6d129d4249d0
jesus santana <jesusjsg@proton.me>
2024-09-09 16:42:28 -0400

    feat: inicializacion de cada tablas

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,116 @@
-const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-        confirmButton: 'btn btn-success m-1',
-        cancelButton: 'btn btn-secondary m-1'
-    },
-    buttonsStyling: false
-})
+import { getDatatable } from "./components/datatable.js";
+import { AJAX_TABLES } from "./apiAjax.js";
+
+
+const tableConductor = document.querySelector('#table-conductor')
+const tableUsuario = document.querySelector('#table-usuario')
+const tableGeneral = document.querySelector('#table-general')
+const tableVehiculo = document.querySelector('#table-vehiculo')
+const tableRuta = document.querySelector('#table-ruta')
+
+function main(){
+    renderTables()
+}
+
+function renderTables(){ // render all tables
+    initConductorTable()
+    initUsuarioTable()
+    initGeneralTable()
+    initVehiculoTable()
+    initRutaTable()
+}
+
+function initConductorTable(){
+    getDatatable(tableConductor, AJAX_TABLES.conductor, [ // conductor table
+        {'data': 'id_conductor'},
+        {'data': 'nombre_conductor'},
+        {'data': 'cedula_conductor'},
+        {'data': 'telefono_conductor'},
+        {'data': 'id_vehiculo'},
+        {'data': 'vencimiento_cedula', visible:false},
+        {'data': 'vencimiento_licencia', visible:false},
+        {'data': 'vencimiento_certificado_medico', visible:false},
+        {'data': 'vencimiento_mppps', visible:false},
+        {'data': 'vencimiento_saberes', visible:false},
+        {'data': 'vencimiento_manejo_seguro', visible:false},
+        {'data': 'vencimiento_alimento', visible:false},
+        {'data': 'tipo_nomina'},
+        {'data': 'opciones', 'className': 'dt-center'}
+    ])
+}
+
+function initUsuarioTable(){
+    getDatatable(tableUsuario, AJAX_TABLES.usuario, [ // usuario table
+        {'data': 'id_usuario', visible:false},
+        {'data': 'nombre_apellido'},
+        {'data': 'nombre_usuario'},
+        {'data': 'nombre_rol', 'className':'dt-center'},
+        {'data': 'opciones', 'className':'dt-center'},
+    ])
+}
+
+function initGeneralTable(){
+    getDatatable(tableGeneral, AJAX_TABLES.general, [ // general table
+        {'data': 'id_registro'},
+        {'data': 'id_entidad'},
+        {'data': 'descripcion1'},
+        {'data': 'descripcion2'},
+        {'data': 'descripcion3'},
+        {'data': 'valor', 'className':'dt-right'},
+        {'data': 'opciones'}
+    ])
+}
+
+function initVehiculoTable(){
+    getDatatable(tableVehiculo, AJAX_TABLES.vehiculo, [ // vehiculo table
+        {'data': 'id_vehiculo'},
+        {'data': 'tipo_vehiculo'},
+        {'data': 'propiedad'},
+        {'data': 'unidad_negocio'},
+        {'data': 'marca'},
+        {'data': 'modelo'},
+        {'data': 'year_vehiculo', visible:false},
+        {'data': 'serial_carroceria', visible:false},
+        {'data': 'serial_motor', visible:false},
+        {'data': 'numero_ejes', visible:false},
+        {'data': 'capacidad_carga', visible:false},
+        {'data': 'uso'},
+        {'data': 'vencimiento_poliza', visible:false},
+        {'data': 'vencimiento_racda', visible:false},
+        {'data': 'vencimiento_sanitario', visible:false},
+        {'data': 'vencimiento_rotc', visible:false},
+        {'data': 'fecha_fumigacion', visible:false},
+        {'data': 'fecha_impuesto', visible:false},
+        {'data': 'bolipuertos', visible:false},
+        {'data': 'gps', visible:false},
+        {'data': 'link_gps', visible:false},
+        {'data': 'estatus_vehiculo'},
+        {'data': 'id_municipio', visible:false},
+        {'data': 'activo_uno'},
+        {'data': 'activo_dos'},
+        {'data': 'activo_tres'},
+        {'data': 'opciones'}
+    ])
+}
+
+function initRutaTable(){
+    getDatatable(tableRuta, AJAX_TABLES.ruta, [ // ruta table
+        {'data': 'id_ruta'},
+        {'data': 'nombre_ruta'},
+        {'data': 'origen'},
+        {'data': 'destino'},
+        {'data': 'kilometros'},
+        {'data': 'opciones'}
+    ])
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+
+
+
+/* import { alertMessages } from "./alertMessages.js";
+
 
 export function formAjaxHandler(form){
     //const formAjax = document.querySelectorAll('.form-ajax')
@@ -17,7 +123,7 @@ export function formAjaxHandler(form){
                 text: '¿Deseas realizar la siguiente operación?',
                 icon: 'question',
                 showCancelButton: true,
-                confirmButtonText: "Sí, Guardar",
+                confirmButtonText: "Sí, Aceptar",
                 cancelButtonText: "No, Cancelar",
                 reverseButtons: true
             }).then((result) => {
@@ -40,7 +146,7 @@ export function formAjaxHandler(form){
                     fetch(action, config)
                     .then(response => response.json())
                     .then(response => {
-                        return alertsAjax(response)
+                        return alertMessages(response)
                     })
                 }
     
@@ -85,27 +191,4 @@ function alertsAjax(alert){
         window.location.href = alert.url
     }
 }
-
-// Funcion para el sidebar 
-(function () {
-	"use strict";
-
-	const treeviewMenu = $('.app-menu');
-
-	// Toggle Sidebar
-	$('[data-toggle="sidebar"]').click(function(event) {
-		event.preventDefault();
-		$('.app').toggleClass('sidenav-toggled');
-	});
-
-	// Activate sidebar treeview toggle
-	$("[data-toggle='treeview']").click(function(event) {
-		event.preventDefault();
-		if(!$(this).parent().hasClass('is-expanded')) {
-			treeviewMenu.find("[data-toggle='treeview']").parent().removeClass('is-expanded');
-		}
-		$(this).parent().toggleClass('is-expanded');
-	});
-
-})();
-
+ */
