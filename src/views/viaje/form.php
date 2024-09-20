02b77e8f9d56b31dbe54a988519c1769bba8c689
jesus santana <jesusjsg@proton.me>
2024-09-20 16:51:53 -0400

    feat: se eliminaron los inputs del detalle

--- src/views/viaje/form.php
+++ src/views/viaje/form.php
@@ -17,7 +17,7 @@ $tipocarga = $generalController->getRegistro($registros['tipoCarga']);
         <h1 class="fw-light mb-3"><i class="bi bi-cursor-fill me-2"></i>Agregar viaje</h1>
         <ul class="app-breadcrumb breadcrumb">
             <li class="breadcrumb-item"><i class="bi bi-cursor-fill"></i></li>
-            <li class="breadcrumb-item"><a class="text-decoration-none" href="<?=URL;?>viaje/">Viajes registrados</a></li>
+            <li class="breadcrumb-item"><a class="text-decoration-none " href="<?=URL;?>viaje/">Viajes registrados</a></li>
         </ul>
     </div>
     <div class="tile">
@@ -117,33 +117,18 @@ $tipocarga = $generalController->getRegistro($registros['tipoCarga']);
     </div>
     <div class="tile">
         <div class="tile-body">
-            <form class="row g-3 form-ajax" action="" method="post" $generalController="off">
+            <form class="row g-3 form-ajax" action="" method="post" autocomplete="off">
                 <div class="d-grip gap-2 d-md-flex justify-content-between">
                     <h3 class="fw-light">Agregar movimientos</h3>
                     <div>
-                        <button type="reset" class="btn btn-primary btn-sm">Limpiar<i class="bi bi-archive ms-1"></i></button>
+                        <button type="button" class="btn btn-primary btn-sm" id="add-row">Agregar fila<i class="bi bi-plus-circle ms-1"></i></button>
+                        <button type="reset" class="btn btn-secondary btn-sm" disabled>Limpiar<i class="bi bi-archive ms-1"></i></button>
                         <button type="submit" class="btn btn-success btn-sm">Guardar<i class="bi bi-floppy ms-1"></i></button>
                     </div>
                 </div>
-                <div class="tile-footer"></div>
-                    <div class="col-md-4">
-                        <label for="origen" class="form-label">Origen</label>
-                        <input type="text" class="form-control" name="origen" id="origen" />
-                    </div>
-                    <div class="col-md-4">
-                        <label for="destino" class="form-label">Destino</label>
-                        <input type="text" class="form-control" name="destino" />
-                    </div>
-                    <div class="col-md-1">
-                        <label for="ruta" class="form-label">Ruta</label>
-                        <input type="text" class="form-control block-input" name="ruta" disabled/>
-                    </div>
-                    <div class="col-md-1">
-                        <label for="km" class="form-label">KM</label>
-                        <input type="text" class="form-control block-input" name="km" disabled/>
-                    </div>
+                <div id="add-movements" class="row g-3">
+
                 </div>
-                <!-- <div class="add-movimientos"></div> -->
                 <div class="tile-footer"></div>
             </form>
         </div>
