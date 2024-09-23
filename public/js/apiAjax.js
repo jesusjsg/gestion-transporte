b5da89959db44e709f686c0177e2eb27df71a983
jesus santana <jesusjsg@proton.me>
2024-09-23 16:54:50 -0400

    feat: path de la url para el javascript

--- public/js/apiAjax.js
+++ public/js/apiAjax.js
@@ -1,19 +1,21 @@
+const BASE_URL = 'http://localhost/gestion-transporte/';
+
 export const AJAX_TABLES = {
-    viaje: 'http://localhost/gestion-transporte/ajax/viaje?action=load_viaje',
-    conductor: 'http://localhost/gestion-transporte/ajax/conductor?action=load_conductores',
-    general: 'http://localhost/gestion-transporte/ajax/general?action=load_general',
-    vehiculo: 'http://localhost/gestion-transporte/ajax/vehiculo?action=load_vehiculos',
-    ruta: 'http://localhost/gestion-transporte/ajax/ruta?action=load_ruta',
-    usuario: 'http://localhost/gestion-transporte/ajax/usuarios?action=load_users',
-}
+    viaje: `${BASE_URL}ajax/viaje?action=load_viaje`,
+    conductor: `${BASE_URL}ajax/conductor?action=load_conductores`,
+    general: `${BASE_URL}ajax/general?action=load_general`,
+    vehiculo: `${BASE_URL}ajax/vehiculo?action=load_vehiculos`,
+    ruta: `${BASE_URL}ajax/ruta?action=load_ruta`,
+    usuario: `${BASE_URL}ajax/usuarios?action=load_users`,
+};
 
 export const AJAX_AUTOCOMPLETE = {
-    cliente: 'http://localhost/gestion-transporte/ajax/viaje?action=get_cliente',
-    municipio: 'http://localhost/gestion-transporte/ajax/ruta?action=get_municipio',
-    conductor: 'http://localhost/gestion-transporte/ajax/viaje?action=get_conductor',
-    placaVehiculo: 'http://localhost/gestion-transporte/ajax/conductor?action=get_placa',
-}
+    cliente: `${BASE_URL}ajax/viaje?action=get_cliente`,
+    municipio: `${BASE_URL}ajax/ruta?action=get_municipio`,
+    conductor: `${BASE_URL}ajax/viaje?action=get_conductor`,
+    placaVehiculo: `${BASE_URL}ajax/conductor?action=get_placa`,
+};
 
 export const AJAX_KILOMETERS = {
-    ruta: 'http://localhost/gestion-transporte/ajax/ruta?action=get_kilometers',
-}
+    ruta: `${BASE_URL}ajax/ruta?action=get_kilometers`,
+};
