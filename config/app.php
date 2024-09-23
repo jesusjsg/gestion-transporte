3853fde2f61f3be9cc909fb51505dbd49ade2a7c
jesus santana <jesusjsg@proton.me>
2024-09-23 16:52:51 -0400

    feat: se elimino el archivo server

--- config/app.php
+++ config/app.php
@@ -1,7 +1,5 @@
 <?php
-
-    define('URL', 'http://localhost/gestion-transporte/');
     define('APP_NAME', 'Gestión Transporte');
+    define('URL', 'http://localhost/gestion-transporte/');
     define('APP_SESION_NAME', 'nomina');
-    define('CHARSET', 'utf8mb4');
     date_default_timezone_set("America/Caracas");
--- config/server.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    define('HOST', 'localhost');
-    define('DB', 'gestion_transporte');
-    define('USER', 'root');
-    define('PASSWORD', '');
