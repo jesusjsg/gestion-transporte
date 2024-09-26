8d82722d6a88870177a19a0a455e62260e7689f0
jesus santana <jesusjsg@proton.me>
2024-09-26 12:14:53 -0400

    fix: se agrego el charset de la web

--- config/app.php
+++ config/app.php
@@ -2,4 +2,5 @@
     define('APP_NAME', 'Gestión Transporte');
     define('URL', 'http://localhost/gestion-transporte/');
     define('APP_SESION_NAME', 'nomina');
+    define('CHARSET', 'utf8');
     date_default_timezone_set("America/Caracas");
