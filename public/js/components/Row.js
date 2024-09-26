38cd1e10ec53c33ddc6673941f2552a81667353a
jesus santana <jesusjsg@proton.me>
2024-09-26 16:17:28 -0400

    perf: se elimino la funcion para eliminar filas

--- public/js/components/Row.js
+++ public/js/components/Row.js
@@ -1,42 +1,29 @@
-export function Row(){
+export function Row(numberMovements){
     return `
         <div class="movements">
-            <div class="row g-3">
+            <div class="row g-3 align-items-center">
                 <div class="hidden">
-                    <input type="hidden" id="number-movements" value="${numberMovement}" />
-                </div>
-                <div class="col-md-4">
-                    <label for="origen" class="form-label">Origen</label>
-                    <input type="text" class="form-control" name="origen[]" />
-                </div>
-                <div class="col-md-4">
-                    <label for="destino" class="form-label">Destino</label>
-                    <input type="text" class="form-control" name="destino[]" />
+                    <input type="hidden" id="number-movements" />
                 </div>
                 <div class="col-md-1">
-                    <label for="codigo-ruta" class="form-label">Ruta</label>
-                    <input type="text" class="form-control" name="codigo-ruta[]" />
+                    <span class="badge text-bg-secondary">N° ${numberMovements}</span>
+                </div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" name="origen[]" placeholder="Ingrese el origen" />
+                </div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control" name="destino[]" placeholder="Ingrese el destino" />
+                </div>
+                <div class="col-md-1">
+                    <input type="text" class="form-control block-input" name="codigo-ruta[]" placeholder="Ruta" disabled />
                 </div>
                 <div class=col-md-1>
-                    <label for="kilometros" class="form-label">KM</label>
-                    <input type="text" class="form-control" name="kilometros-movimiento[]" />
+                    <input type="text" class="form-control block-input" name="kilometros-movimiento[]" placeholder="Km" disabled />
                 </div>
-                <div class="col-md-1">
+                <div class="col-auto">
                     <button type="button" class="btn btn-danger remove-row" id="delete-row">X</button>
                 </div>        
             </div>
         </div>
     `
-}
-
-export function deleteRow(containerID){
-    const container = containerID
-    container.addEventListener('click', function(event){
-        if(event.target.classList.contains('remove-row')){
-            const row = event.target.closest('.movements')
-            if(row){
-                row.remove()
-            }
-        }
-    })
 }
