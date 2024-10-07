7e9b56ba1f0eb290fb85fd8eaa7f57df453f82df
jesus santana <jesusjsg@proton.me>
2024-10-07 16:43:38 -0400

    perf: crud vehiculo finalizado

--- src/controllers/vehiculoController.php
+++ src/controllers/vehiculoController.php
@@ -49,7 +49,7 @@ class vehiculoController extends uniqueModel
 
         if (!empty($linkGps)) {
             if (!filter_var($linkGps, FILTER_VALIDATE_URL)) {
-                return $this->errorHandler('El link del GPS solo puede contener URLs.');
+                return $this->errorHandler('El link del GPS solo puede contener una URL válida.');
             }
         }
 
@@ -258,6 +258,7 @@ class vehiculoController extends uniqueModel
                 }
 
                 $row['opciones'] = '
+                        <a href="'.URL.'vehiculo/editar/'.$row['id_vehiculo'].'/" class="btn btn-primary btn-sm"><i class="bi bi-pencil-square m-0 p-0"></i></a>
                         <form class="form-ajax d-inline" action="' . URL . 'ajax/vehiculo" method="post" autocomplete="off">
                             <input type="hidden" name="model_vehiculo" value="delete" />
                             <input type="hidden" name="id-vehiculo" value="' . $row['id_vehiculo'] . '" />
@@ -273,7 +274,217 @@ class vehiculoController extends uniqueModel
     }
 
     public function updateVehiculo()
-    {}
+    {
+        $idVehiculo = $this->cleanString($_POST['id-vehiculo']);
+
+        $data = $this->executeQuery("SELECT * FROM vehiculos WHERE id_vehiculo='$idVehiculo'");
+
+        if ($data->rowCount() <= 0) {
+            return $this->errorHandler('No hemos encontrado el vehículo en el sistema.');
+        } else {
+            $data = $data->fetch();
+        }
+
+        $placa = $this->cleanString($_POST['placa']);
+        $tipoVehiculo = $this->cleanString($_POST['tipo-vehiculo']);
+        $propiedad = $this->cleanString($_POST['propiedad']);
+        $unidadNegocio = $this->cleanString($_POST['unidad-negocio']);
+        $marca = $this->cleanString($_POST['marca-vehiculo']);
+        $modelo = $this->cleanString($_POST['modelo-vehiculo']);
+        $year = $this->cleanString($_POST['year-vehiculo']);
+        $serialCarroceria = $this->cleanString($_POST['serial-carroceria']);
+        $serialMotor = $this->cleanString($_POST['serial-motor']);
+        $numeroEjes = $this->cleanString($_POST['numero-ejes']);
+        $capacidadCarga = $this->cleanString($_POST['capacidad-carga']);
+        $uso = $this->cleanString($_POST['uso-vehiculo']);
+        $bolipuertos = $this->cleanString($_POST['bolipuertos']);
+        $gps = $this->cleanString($_POST['gps']);
+        $linkGps = $this->cleanString($_POST['link-gps']);
+        $estatus = $this->cleanString($_POST['estatus-vehiculo']);
+        $municipio = $this->cleanString($_POST['id-municipio']);
+        $activoSapUno = $this->cleanString($_POST['activo-uno']);
+        $activoSapDos = $this->cleanString($_POST['activo-dos']);
+        $activoSapTres = $this->cleanString($_POST['activo-tres']);
+        
+        $vencimientoPoliza = !empty($_POST['vencimiento-cedula']) ? $this->cleanString($_POST['vencimiento-cedula']) : null;
+        $vencimientoRacda = !empty($_POST['vencimiento-racda']) ? $this->cleanString($_POST['vencimiento-racda']) : null;
+        $vencimientoSanitario = !empty($_POST['vencimiento-sanitario']) ? $this->cleanString($_POST['vencimiento-sanitario']) : null;
+        $vencimientoRotc = !empty($_POST['vencimiento-rotc']) ? $this->cleanString($_POST['vencimiento-rotc']) : null;
+        $fechaFumigacion = !empty($_POST['fecha-fumigacion']) ? $this->cleanString($_POST['fecha-fumigacion']) : null;
+        $fechaImpuestos = !empty($_POST['fecha-impuestos']) ? $this->cleanString($_POST['fecha-impuestos']) : null;
+
+        if (empty($placa) || empty($tipoVehiculo) || empty($serialCarroceria) || empty($serialMotor) || empty($modelo)) {
+            return $this->errorHandler('Todos los campos son obligatorios.');
+        }
+
+        if ($this->verifyData('[A-Za-z0-9]{7,8}', $placa)) {
+            return $this->errorHandler('La placa solo puede contener números con un rango de 7 a 8 digitos.');
+        }
+
+        if (!empty($linkGps)) {
+            if (!filter_var($linkGps, FILTER_VALIDATE_URL)) {
+                return $this->errorHandler('El link del GPS solo puede contener una URL válida.');
+            }
+        }
+
+        $checkPlaca = $this->executeQuery("SELECT id_vehiculo FROM vehiculos WHERE id_vehiculo = '$placa'");
+        if ($checkPlaca->rowCount() > 0) {
+            return $this->errorHandler('La placa del vehículo se encuentra registrada.');
+        }
+
+        $vehiculoDataUpdate = [
+            [
+                'field_name_database' => 'id_vehiculo',
+                'field_name_form' => 'placa',
+                'field_value' => $placa,
+            ],
+            [
+                'field_name_database' => 'tipo_vehiculo',
+                'field_name_form' => 'tipoVehiculo',
+                'field_value' => $tipoVehiculo,
+            ],
+            [
+                'field_name_database' => 'propiedad',
+                'field_name_form' => 'propiedad',
+                'field_value' => $propiedad,
+            ],
+            [
+                'field_name_database' => 'unidad_negocio',
+                'field_name_form' => 'unidadNegocio',
+                'field_value' => $unidadNegocio,
+            ],
+            [
+                'field_name_database' => 'marca',
+                'field_name_form' => 'marca',
+                'field_value' => $marca,
+            ],
+            [
+                'field_name_database' => 'modelo',
+                'field_name_form' => 'modelo',
+                'field_value' => $modelo,
+            ],
+            [
+                'field_name_database' => 'year_vehiculo',
+                'field_name_form' => 'year',
+                'field_value' => $year,
+            ],
+            [
+                'field_name_database' => 'serial_carroceria',
+                'field_name_form' => 'serialCarroceria',
+                'field_value' => $serialCarroceria,
+            ],
+            [
+                'field_name_database' => 'serial_motor',
+                'field_name_form' => 'serialMotor',
+                'field_value' => $serialMotor,
+            ],
+            [
+                'field_name_database' => 'numero_ejes',
+                'field_name_form' => 'numeroEjes',
+                'field_value' => $numeroEjes,
+            ],
+            [
+                'field_name_database' => 'capacidad_carga',
+                'field_name_form' => 'capacidadCarga',
+                'field_value' => $capacidadCarga,
+            ],
+            [
+                'field_name_database' => 'uso',
+                'field_name_form' => 'uso',
+                'field_value' => $uso,
+            ],
+            [
+                'field_name_database' => 'vencimiento_poliza',
+                'field_name_form' => 'vencimientoPoliza',
+                'field_value' => $vencimientoPoliza,
+            ],
+            [
+                'field_name_database' => 'vencimiento_racda',
+                'field_name_form' => 'vencimientoRacda',
+                'field_value' => $vencimientoRacda,
+            ],
+            [
+                'field_name_database' => 'vencimiento_sanitario',
+                'field_name_form' => 'vencimientoSanitario',
+                'field_value' => $vencimientoSanitario,
+            ],
+            [
+                'field_name_database' => 'vencimiento_rotc',
+                'field_name_form' => 'vencimientoRotc',
+                'field_value' => $vencimientoRotc,
+            ],
+            [
+                'field_name_database' => 'fecha_fumigacion',
+                'field_name_form' => 'fechaFumigacion',
+                'field_value' => $fechaFumigacion,
+            ],
+            [
+                'field_name_database' => 'fecha_impuesto',
+                'field_name_form' => 'fechaImpuestos',
+                'field_value' => $fechaImpuestos,
+            ],
+            [
+                'field_name_database' => 'bolipuertos',
+                'field_name_form' => 'bolipuertos',
+                'field_value' => $bolipuertos,
+            ],
+            [
+                'field_name_database' => 'gps',
+                'field_name_form' => 'gps',
+                'field_value' => $gps,
+            ],
+            [
+                'field_name_database' => 'link_gps',
+                'field_name_form' => 'linkGps',
+                'field_value' => $linkGps,
+            ],
+            [
+                'field_name_database' => 'estatus_vehiculo',
+                'field_name_form' => 'estatus',
+                'field_value' => $estatus,
+            ],
+            [
+                'field_name_database' => 'id_municipio',
+                'field_name_form' => 'municipio',
+                'field_value' => $municipio,
+            ],
+            [
+                'field_name_database' => 'activo_uno',
+                'field_name_form' => 'sap1',
+                'field_value' => $activoSapUno,
+            ],
+            [
+                'field_name_database' => 'activo_dos',
+                'field_name_form' => 'sap2',
+                'field_value' => $activoSapDos,
+            ],
+            [
+                'field_name_database' => 'activo_tres',
+                'field_name_form' => 'sap3',
+                'field_value' => $activoSapTres,
+            ],
+        ];
+
+        $condition = [
+            "condition_field" => "id_vehiculo",
+            "condition_marker" => "id_vehiculo",
+            "condition_value" => $placa,
+        ];
+
+        if ($this->updateData('conductores', $vehiculoDataUpdate, $condition)) {
+            if ($placa == isset($_SESSION['id_vehiculo'])) {
+                $_SESSION['id_vehiculo'] = $placa;
+            }
+
+            return $this->successHandler(
+                'reload',
+                'El vehículo (' . ucwords($placa) . ') ha sido actualizado correctamente.',
+                'Conductor actualizado',
+            );
+        } else {
+            return $this->errorHandler('No se pudo actualizar el vehículo.');
+        }
+    }
 
     public function deleteVehiculo()
     {
