70431a52045a16818e919d21a41b433f4eb1f6ae
jesus santana <jesusjsg@proton.me>
2024-10-10 16:44:01 -0400

    feat: se agregaron los metodos crud para las tablas compuestas

--- src/models/doubleModel.php
+++ src/models/doubleModel.php
@@ -1,6 +1,7 @@
 <?php
 namespace src\models;
 
+USE PDO;
 // Clase para el modelo de las tablas que cuenten con una llave primaria compuesta
 class doubleModel extends mainModel
 {
@@ -40,10 +41,61 @@ class doubleModel extends mainModel
     }
 
     public function selectData($type, $table, $idRegistro, $idEntidad)
-    {}
+    {
+        $type = $this->cleanString($type);
+        $table = $this->cleanString($table);
+        $idRegistro = $this->cleanString($idRegistro);
+        $idEntidad = $this->cleanString($idEntidad);
+
+        if ($type == 'Primary') {
+            $sql = $this->conection()->prepare("SELECT * FROM $table WHERE id_registro = :id_registro AND id_entidad = :id_entidad");
+            $sql->bindParam(':id_registro', $idRegistro, PDO::PARAM_STR);
+            $sql->bindParam(':id_entidad', $idEntidad, PDO::PARAM_STR);
+        
+        } else if ($type == 'Normal') {
+            $sql = $this->conection()->prepare("SELECT * FROM $table");
+        }
+        $sql->execute();
+        return $sql;
+    }
 
     protected function updateData($table, $data, $condition)
-    {}
+    {
+        $query = "UPDATE $table SET ";
+        $count = 0;
+
+        foreach ($data as $key) {
+            if ($count >= 1) {
+                $query .= ", ";
+            }
+            $query .= $key['field_name_database'] . ' = :' . $key['field_name_form'];
+            $count++;
+        }
+
+        $query .= " WHERE ";
+        $conditionCount = 0;
+        foreach ($condition as $cond) {
+            if ($conditionCount >= 1) {
+                $query .= " AND ";
+            }
+            $query .= $cond['condition_field'] . " = :" . $cond['condition_marker'];
+            $conditionCount++;
+        }
+
+        $sql = $this->conection()->prepare($query);
+
+        foreach ($data as $key) {
+            $sql->bindParam(':' . $key['field_name_form'], $key['field_value']);
+        }
+
+        foreach ($condition as $cond) {
+            $sql->bindParam(':' . $cond['condition_marker'], $cond['condition_value']);
+        }
+
+        $sql->execute();
+        return $sql;
+    }
+
 
     protected function deleteData($table, $idRegistro, $idEntidad)
     {
