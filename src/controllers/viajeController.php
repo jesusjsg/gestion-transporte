5f7b8a0755a607c05e4cf47ba17888090c93213e
jesus santana <jesusjsg@proton.me>
2024-10-28 16:52:01 -0400

    feat: registerViaje y tableViaje agregados

--- src/controllers/viajeController.php
+++ src/controllers/viajeController.php
@@ -1,6 +1,7 @@
 <?php
 namespace src\controllers;
 
+use Exception;
 use PDO;
 use src\models\uniqueModel;
 
@@ -9,85 +10,52 @@ class viajeController extends uniqueModel
 
     public function registerViaje()
     {
-
-        $fichaConductor = $this->cleanString($_POST['ficha-conductor']);
-        $nombreConductor = $this->cleanString($_POST['nombre-conductor']);
-        $placaVehiculo = $this->cleanString($_POST['placa-vehiculo']);
-        $operacion = $this->cleanString($_POST['tipo-operacion']);
-        $carga = $this->cleanString($_POST['tipo-carga']);
-        $aviso = intval($this->cleanString($_POST['aviso']));
-        $idCliente = intval($this->cleanString($_POST['id-cliente']));
-        $codigoRuta = $this->cleanString($_POST['codigo-ruta']);
+        $fichaConductor = trim($this->cleanString($_POST['ficha-conductor']));
+        $idCliente = trim($this->cleanString($_POST['codigo-cliente']));
+        $placaVehiculo = trim($this->cleanString($_POST['placa-vehiculo']));
+        $tipoOperacion = $this->cleanString($_POST['tipo-operacion']);
+        $tipoCarga = $this->cleanString($_POST['tipo-carga']);
+        $aviso = $this->cleanString($_POST['aviso']);
         $fechaInicio = $this->cleanString($_POST['fecha-inicio']);
         $fechaCierre = $this->cleanString($_POST['fecha-cierre']);
-        $sabado = intval($this->cleanString($_POST['sabados']));
-        $domingo = intval($this->cleanString($_POST['domingos']));
-        $feriado = intval($this->cleanString($_POST['feriados']));
-        $tasaCambio = floatval($this->cleanString($_POST['tasa-cambio']));
-        $montoUsd = floatval($this->cleanString($_POST['monto-usd']));
-        $montoVes = floatval($this->cleanString($_POST['monto-ves']));
-        $kilometros = intval($this->cleanString($_POST['kilometros']));
+        $numeroNomina = $this->cleanString($_POST['numero-nomina']);
+        $feriados = trim($this->cleanString($_POST['cantidad-feriados']));
+        $sabados = $this->cleanString($_POST['cantidad-sabados']);
+        $domingos = $this->cleanString($_POST['cantidad-domingos']);
+        $tasaCambio = $this->cleanString($_POST['tasa-cambio']);
 
-        if (empty($nombreConductor) || empty($placaVehiculo) || empty($aviso) || empty($feriado)) {
-            $alert = [
-                'type' => 'simple',
-                'icon' => 'error',
-                'title' => 'Ocurrió un error',
-                'text' => 'Todos los campos son necesarios.',
-            ];
-            return json_encode($alert);
+        if (empty($fichaConductor)) {
+            return $this->errorHandler('El nombre del conductor es obligatorio.');
         }
 
-        if ($this->verifyData('[a-zA-Z ]{10,255}', $nombreConductor)) {
-            $alert = [
-                'type' => 'simple',
-                'icon' => 'error',
-                'title' => 'Ocurrió un error',
-                'text' => 'El nombre del conductor solo puede contener caracteres.',
-            ];
-            return json_encode($alert);
+        if (empty($placaVehiculo)) {
+            return $this->errorHandler('La placa del vehículo es obligatoria.');
         }
 
-        if ($this->verifyData('[0-9]{10}', $aviso) || $aviso < 0) {
-            $alert = [
-                'type' => 'simple',
-                'icon' => 'error',
-                'title' => 'Ocurrió un error',
-                'text' => 'El aviso solo puede contener números enteros con un mínimo de 10 digitos.',
-            ];
-            return json_encode($alert);
+        if (empty($idCliente)) {
+            return $this->errorHandler('El cliente es obligatorio.');
         }
 
-        if ($this->verifyData('[0-9]', $feriado) || $feriado < 0) {
-            $alert = [
-                'type' => 'simple',
-                'icon' => 'error',
-                'title' => 'Ocurrió un error',
-                'text' => 'El feriado solo puede contener números enteros.',
-            ];
-            return json_encode($alert);
-        }
-
-        $dataViajeLog = [
+        $viajeDataLog = [
             [
                 'field_name_database' => 'id_conductor',
-                'field_name_form' => ':fichaConductor',
+                'field_name_form' => ':ficha',
                 'field_value' => $fichaConductor,
             ],
             [
                 'field_name_database' => 'id_vehiculo',
-                'field_name_form' => ':placaVehiculo',
+                'field_name_form' => ':placa',
                 'field_value' => $placaVehiculo,
             ],
             [
                 'field_name_database' => 'id_tipo_operacion',
-                'field_name_form' => ':tipoOperacion',
-                'field_value' => $operacion,
+                'field_name_form' => ':operacion',
+                'field_value' => $tipoOperacion,
             ],
             [
                 'field_name_database' => 'id_tipo_carga',
-                'field_name_form' => ':tipoCarga',
-                'field_value' => $carga,
+                'field_name_form' => ':carga',
+                'field_value' => $tipoCarga,
             ],
             [
                 'field_name_database' => 'aviso',
@@ -96,13 +64,13 @@ class viajeController extends uniqueModel
             ],
             [
                 'field_name_database' => 'id_cliente',
-                'field_name_form' => ':idCliente',
+                'field_name_form' => ':cliente',
                 'field_value' => $idCliente,
             ],
             [
-                'field_name_database' => 'id_ruta',
-                'field_name_form' => ':idRuta',
-                'field_value' => $codigoRuta,
+                'field_name_database' => 'nro_nomina',
+                'field_name_form' => ':nomina',
+                'field_value' => $numeroNomina,
             ],
             [
                 'field_name_database' => 'fecha_inicio',
@@ -117,118 +85,86 @@ class viajeController extends uniqueModel
             [
                 'field_name_database' => 'sabados',
                 'field_name_form' => ':sabados',
-                'field_value' => $sabado,
+                'field_value' => $sabados,
             ],
             [
                 'field_name_database' => 'domingos',
                 'field_name_form' => ':domingos',
-                'field_value' => $domingo,
+                'field_value' => $domingos,
             ],
             [
                 'field_name_database' => 'feriados',
                 'field_name_form' => ':feriados',
-                'field_value' => $feriado,
-            ],
-            [
-                'field_name_database' => 'tasa_cambio',
-                'field_name_form' => ':tasaCambio',
-                'field_value' => $tasaCambio,
-            ],
-            [
-                'field_name_database' => 'monto_usd',
-                'field_name_form' => ':montoUsd',
-                'field_value' => $montoUsd,
-            ],
-            [
-                'field_name_database' => 'monto_ves',
-                'field_name_form' => ':montoVes',
-                'field_value' => $montoVes,
-            ],
-            [
-                'field_name_database' => 'kilometros',
-                'field_name_form' => ':totalKilometros',
-                'field_value' => $kilometros,
-            ],
+                'field_value' => $feriados,
+            ]
         ];
 
-        $saveViaje = $this->saveData('viajes', $dataViajeLog);
+        $saveViaje = $this->saveData('viajes', $viajeDataLog);
 
         if ($saveViaje->rowCount() == 1) {
-            $alert = [
-                'type' => 'reload',
-                'icon' => 'success',
-                'title' => 'Viaje registrado',
-                'text' => 'El viaje ha sido registrado correctamente.',
-            ];
+            return $this->successHandler('simple', 'El viaje ha sido registrado correctamente.');
         } else {
-            $alert = [
-                'type' => 'simple',
-                'icon' => 'error',
-                'title' => 'Ocurrió un error',
-                'text' => 'Hubo un problema al registrar el viaje.',
-            ];
+            return $this->errorHandler('Hubo un problema al registrar el viaje.');
         }
-        return json_encode($alert);
     }
 
     public function tableViaje()
     {
-
-        $getTableViaje = $this->executeQuery(
+        $getTableViajes = $this->executeQuery(
             "SELECT
-                    viajes.id_viaje,
-                    viajes.id_conductor,
-                    viajes.id_vehiculo,
-                    viajes.id_tipo_operacion,
-                    viajes.id_tipo_carga,
-                    viajes.aviso,
-                    viajes.id_cliente,
-                    ruta.nombre_ruta,
-                    viajes.fecha_inicio,
-                    viajes.fecha_cierre,
-                    viajes.sabados,
-                    viajes.domingos,
-                    viajes.feriados,
-                    viajes.monto_usd,
-                    viajes.monto_ves,
-                    viajes.total_kilometros,
-                    id_operacion.descripcion1 AS id_tipo_operacion,
-                    id_carga.descripcion1 AS id_tipo_carga,
-                    cliente.descripcion1 AS id_cliente
-                FROM viajes
-                LEFT JOIN
-                    general AS id_operacion ON viajes.id_tipo_operacion = id_operacion.id_entidad AND id_operacion.id_registro = 3
-                LEFT JOIN
-                    general AS id_carga ON viajes.id_tipo_carga = id_carga.id_entidad AND id_carga.id_registro = 4
-                LEFT JOIN
-                    general AS cliente ON viajes.id_cliente = cliente.id_entidad AND cliente.id_registro = 7
-                INNER JOIN
-                    rutas AS ruta ON viajes.id_ruta = ruta.id_ruta
-                "
+                conductor.nombre_conductor,
+                viajes.id_vehiculo,
+                operacion.descripcion1 AS id_tipo_operacion,
+                carga.descripcion1 AS id_tipo_carga,
+                viajes.aviso,
+                cliente.descripcion1 AS id_cliente,
+                ruta.nombre_ruta,
+                viajes.fecha_inicio,
+                viajes.fecha_cierre,
+                viajes.sabados,
+                viajes.domingos,
+                viajes.feriados,
+                viajes.nro_nomina,
+                viajes.tasa_cambio
+            FROM viajes
+            LEFT JOIN
+                general AS operacion ON viajes.id_tipo_operacion = operacion.id_entidad AND operacion.id_registro = 3
+            LEFT JOIN
+                general AS carga ON viajes.id_tipo_carga = carga.id_entidad AND carga.id_registro = 4
+            LEFT JOIN
+                general AS cliente ON viajes.id_cliente = cliente.id_entidad AND cliente.id_registro = 7
+            INNER JOIN
+                rutas AS ruta ON viajes.id_ruta = ruta.id_ruta
+            INNER JOIN
+                conductores AS conductor ON viajes.id_conductor = conductor.id_conductor
+            "
         );
+
         $data = [];
         $dateColumns = [
             'fecha_inicio',
             'fecha_cierre',
         ];
 
-        if ($getTableViaje->rowCount() > 0) {
-            while ($row = $getTableViaje->fetch(PDO::FETCH_ASSOC)) {
-                foreach ($dateColumns as $column) {
-                    if (!empty($row[$column])) {
-                        $row[$column] = $this->formatDate($row[$column]);
+        if ($getTableViajes->rowCount() > 0) {
+            while ($row = $getTableViajes->fetch(PDO::FETCH_ASSOC)) {
+                foreach ($dateColumns as $date) {
+                    if (!empty($row[$date])) {
+                        $row[$date] = $this->formatDate($row[$date]);
                     }
                 }
 
                 foreach ($row as $key => $value) {
                     if (empty($value)) {
-                        $row[$key] = '<span class="badge text-bg-secondary">No definido</span>';
+                        $row[$key] = '<span class="badge text-bg-secondary">No definido</span>'; 
                     }
                 }
+
                 $row['opciones'] = '
-                        <form class="form-ajax d-inline" action="' . URL . 'ajax/viaje" method="post" autocomplete="off">
+                        <a href="'.URL.'viajes/editar/'.$row['id_viaje'].'/" class="btn btn-primary btn-sm"><i class="bi bi-pencil-square m-0 p-0"></i></a>
+                        <form class="form-ajax d-inline" action="'.URL.'ajax/viaje" method="post" autocomplete="off">
                             <input type="hidden" name="model_viaje" value="delete" />
-                            <input type="hidden" name="id-viaje" value="' . $row['id_viaje'] . '" />
+                            <input type="hidden" name="id-viaje" value="'.$row['id_viaje'].'" />
                             <button type="submit" class="btn btn-danger btn-sm">
                                 <i class="bi bi-trash3 m-0 p-0"></i>
                             </button>
@@ -248,8 +184,13 @@ class viajeController extends uniqueModel
 
     public function totalViajes()
     {
-        $sql = $this->executeQuery("SELECT COUNT(*) AS total FROM viajes");
-        $totalViajes = $sql->fetchColumn();
-        return $totalViajes;
+        try {
+            $sql = $this->executeQuery("SELECT COUNT(*) AS total FROM viajes");
+            $totalViajes = $sql->fetchColumn();
+            return $totalViajes;
+
+        } catch (Exception $error) {
+
+        }
     }
 }
